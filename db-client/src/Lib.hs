988df25ba59567c7231a5d92cd1d2f36d6ab6c82
clojurians-org <larluo@clojurians.org>
2018-06-01 16:15:14 +0800

    add oracle support

--- db-client/src/Lib.hs
+++ db-client/src/Lib.hs
@@ -5,7 +5,12 @@ module Lib
 
 import Control.Exception (bracket)
 import Database.MySQL.Base (ConnectInfo(..), Connection, defaultConnectInfo, connect, close)
+import Database.MySQL.Base.Types (Field)
 import Database.MySQL.Simple (Query(..), query_, execute_, Only)
+
+import qualified Database.HDBC as H
+import qualified Database.HDBC.ODBC as HO
+import Database.HDBC.ODBC (connectODBC)
 
 
 mysqlConnInfo = defaultConnectInfo {
@@ -27,3 +32,7 @@ query2 :: IO Connection -> IO [(Int, String)]
 query2 mysqlConn = bracket  mysqlConn close $ \conn ->
   query_ conn "select 1 + 1, 'a'"
 
+oracleConnInfo = "Driver={MyOracle};DBQ=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=10.129.35.238)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=EDWDBUAT)(SERVER=DEDICATED)));UID=fsd;PWD=fsd;"
+
+oracleConn :: IO HO.Connection
+oracleConn = connectODBC oracleConnInfo
