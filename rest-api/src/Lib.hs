42162020d4e18ff4f667ea4dc589b5a72df3de76
clojurians-org <larluo@clojurians.org>
2018-06-04 22:21:36 +0800

    test lens

--- rest-api/src/Lib.hs
+++ rest-api/src/Lib.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Lib
     (libMain
@@ -11,9 +13,10 @@ import Data.Proxy (Proxy(..))
 import Network.Wai.Handler.Warp (run)
 
 import Data.Aeson
-import Control.Lens ((?~), (.~), (&))
+import Control.Lens ((?~), (.~), (&), makeLenses)
 import Servant.API ((:<|>), (:>), Get, JSON, Summary)
 import Servant.Server (Server, serve)
+import Data.Swagger (Swagger, description, info, title, version)
 import Servant.Swagger (toSwagger)
 import Servant.Swagger.UI (SwaggerSchemaUI)
 import Servant.Swagger.UI.ReDoc (redocSchemaUIServer)
@@ -25,10 +28,10 @@ type MyAPI = "myAPI" :> Summary "my API" :> Get '[JSON] String
 type DocReq = ("docs" :> "v1" :> SwaggerSchemaUI "index" "swagger.json")
 type ApiReq = ("api" :> "v1" :> MyAPI)
 
-
-docHandler = (redocSchemaUIServer (toSwagger (Proxy::Proxy MyAPI)) & info.title .~ "My API")
-apiHandler = (redocSchemaUIServer (toSwagger (Proxy::Proxy MyAPI)) & info.title .~ "My API")
+docHandler = redocSchemaUIServer $ toSwagger (Proxy::Proxy MyAPI) & info.title .~ "My API"
+apiHandler = redocSchemaUIServer $ toSwagger (Proxy::Proxy MyAPI) & info.title .~ "My API"
 main = run 1111 $ serve (Proxy::Proxy (DocReq :<|> ApiReq)) (docHandler :<|> apiHandler)
+
 {--
   run 1111 $ serve (Proxy::Proxy UserAPI1) (return "hello world")
 
