db26945dac57f927457ef9f4690ccba67aa8de65
clojurians-org <larluo@clojurians.org>
2019-04-23 18:42:24 +0800

    add oracle to yarml

--- oracle-to-yaml/src/Main.hs
+++ oracle-to-yaml/src/Main.hs
@@ -1,26 +1,33 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Control.Monad.Loops (unfoldM)
 import Database.Dpi (
-    ExecMode(ModeExecDefault)
+    ExecMode(ModeExecDefault), DataValue(..), OracleConfig, SQL
   , defaultOracle, withContext, withConnection, withStatement
-  , executeStatement, fetch, getQueryValue
+  , executeStatement, getRowCount, fetch, getQueryValue, getQueryInfo
   )
-
+import Database.Dpi.Field (
+    DataField(..), FromDataFields(fromDataFields')
+  )
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
 
 
-repl :: IO ()
-repl = do
-  let conf = defaultOracle "KB" "KB123456" "10.132.37.241:1521/EDMP"
+quickQuery :: FromDataFields a => OracleConfig -> SQL -> IO [a]
+quickQuery conf sql = do
+  let fetchRow st cn = mapM (\i -> DataField <$> (getQueryInfo st i) <*> (getQueryValue st i)) [1..cn]
   withContext $ \ctx ->
     withConnection ctx conf return $ \conn ->
-      withStatement conn False "select sysdate from dual" $ \st -> do
-        r <- executeStatement st ModeExecDefault
-        f <- fetch st
-        mapM (getQueryValue st) [1..r] >>= print
--- >>> conn <- connectOracle "KB" "KB123456" "10.132.37.241:1521/EDMP"
--- >>>
+      withStatement conn False sql $ \st -> do
+        cn <- executeStatement st ModeExecDefault
+        allFetch <- unfoldM (fetch st)
+        mapM (const (fetchRow st cn >>= fromDataFields')) allFetch
+        
 
+-- >>> :set -XOverloadedStrings
+-- >>> :reload
+
+-- >>> let dbConf = defaultOracle "KB" "KB123456" "10.132.37.241:1521/EDMP"
+-- >>> quickQuery dbConf "select * from tb_interface where rownum <= 2" :: IO [String]
