9ec19a49f22adcc73fbbd629217a48df9e370269
clojurians-org <larluo@clojurians.org>
2019-05-06 17:19:59 +0800

    backup minio script

---
+++ minio-migration/src/Main.hs
@@ -0,0 +1,47 @@
+module Main where
+
+import Control.Monad.IO.Class (liftIO) 
+import Data.Function ((&))
+import Control.Monad (void, when)
+import Network.Minio (
+    ConnectInfo(..), Credentials(..)
+  , setCreds, setRegion
+  , runMinio, makeBucket, bucketExists, listBuckets
+  , listObjects
+  , fPutObject, defaultPutObjectOptions
+  )
+
+import Conduit (runConduit)
+
+
+myCI :: ConnectInfo
+myCI = "http://10.132.34.83:9000"
+         & setCreds (Credentials "BY2BFHISRTPNY36IR4TD"
+                                 "ZB66/2jxW0bXkiEU0kufFT0ni1tOut9QJG8v1hb7")
+         & setRegion "us-east-1"
+  
+main :: IO ()
+main = do
+  res <- runMinio myCI $ do
+    liftIO $ putStrLn $ "start ..."
+    {--
+    buckets <- listBuckets
+    liftIO $ print $ "Top 5 buckets: " ++ show (take 5 buckets)
+    bExist <- bucketExists "larluo"
+    when (not bExist) $ void $ makeBucket "larluo" Nothing
+    liftIO $ putStrLn $ "--> put tgz.nix-2.2.2"
+
+    fPutObject "larluo"
+      "tgz.nix-2.2.2"
+      "/home/op/my-env/nix.sh.out/tgz.nix-2.2.2"
+      defaultPutObjectOptions
+    --}
+    runConduit $ listObjects "icif.uat" Nothing true
+  case res of
+    Left e -> putStrLn $ "operate failed due to " ++ (show e)
+    Right _ -> putStrLn "done!"
+
+-- >>> main
+-- start ...
+-- put tgz.nix-2.2.2
+-- done!
