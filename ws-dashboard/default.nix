6471a5b9b500edf7f2f03dbbc48cf0dbb41dd54e
clojurians-org <larluo@clojurians.org>
2019-05-22 18:36:19 +0800

    downgrade obelisk version for prerender

--- ws-dashboard/default.nix
+++ ws-dashboard/default.nix
@@ -2,9 +2,16 @@
 , iosSdkVersion ? "10.2"
 }:
 with import ./.obelisk/impl { inherit system iosSdkVersion; };
-project ./. ({ ... }: {
+project ./. ({ hackGet, ... }: {
   android.applicationId = "systems.obsidian.obelisk.examples.minimal";
   android.displayName = "Obelisk Minimal Example";
   ios.bundleIdentifier = "systems.obsidian.obelisk.examples.minimal";
   ios.bundleName = "Obelisk Minimal Example";
+
+  packages = {
+    echarts-jsdom = hackGet ./dep/echarts-jsdom ;
+    reflex-dom-echarts = hackGet ./dep/reflex-dom-echarts ;
+  } ;
 })
+
+
--- ws-dashboard/frontend/src/Frontend.hs
+++ ws-dashboard/frontend/src/Frontend.hs
@@ -2,6 +2,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RankNTypes #-}
 module Frontend where
 
 import Data.Function ((&))
@@ -12,24 +15,38 @@ import Obelisk.Frontend
 import Data.Default (def)
 import qualified Data.Aeson as J
 import Data.ByteString.Lazy (fromStrict)
+import qualified Data.ByteString as B
 
-import Control.Lens ((.~))
+import Control.Lens ((.~), (?~))
+import Control.Monad (void)
 import Reflex (
-    Event, never
+    Reflex(..), MonadSample(..), MonadHold(..)
+  , PerformEvent(..), PostBuild(..), TriggerEvent(..), Event
+  , never, fmapMaybe, sample, constDyn
   )
 import Reflex.Dom.Core (
-    DomBuilder, Prerender(prerender)
+    DomBuilder
+  --, Prerender(prerender)
   , el, text, elAttr, (=:), blank, divClass
   )
+import Reflex.Dom.Prerender (Prerender, prerender)
 import Reflex.Dom.WebSocket (webSocket, webSocketConfig_send, RawWebSocket (_webSocket_recv))
 import Obelisk.Route (R)
 import Obelisk.Route.Frontend (subRoute_)
+
+import Data.Maybe (fromJust)
+import Data.Map (Map, fromList)
+import Data.Time (UTCTime, getCurrentTime)
+import Reflex.Dom.Widget.ECharts (
+    TimeLineChartConfig(..), ChartOptions(..), Series(..), SeriesLine
+  , timeLineChart, series_smooth, series_name
+  )
+import Data.Time (parseTimeM, defaultTimeLocale)
 
 import Common.Api
 import Common.Route
 import Common.Types
 import Obelisk.Generated.Static
-
 
 styleSheet :: DomBuilder t m => Text -> m ()
 styleSheet myLink = elAttr "link" attrs blank
@@ -43,15 +60,35 @@ pageHeader = do
   elAttr "meta" ("charset" =: "utf-8") blank
   elAttr "script" ("type" =: "text/javascript" <> "src" =: static @"echarts.min.js") blank
 
-apiStatWS :: (DomBuilder t m, Prerender js t m) => m (Event t ApiStat)
+apiStatWS :: forall t m js .
+          ( DomBuilder t m, Prerender js m, PerformEvent t m, TriggerEvent t m, PostBuild t m)
+          => m (Event t ApiStat)
 apiStatWS = do
-  wsRespEv <- prerender (return never) $ do
-    ws <- webSocket "" $ def & webSocketConfig_send .~ (never :: Event t [Text])
-    return (_webSocket_recv ws)
+  wsRespEv <-
+    prerender (return never) $ do
+      ws <- webSocket "" $ def & webSocketConfig_send .~ (never :: Event t [Text])
+      return (_webSocket_recv ws)
   return $ fmapMaybe (J.decode . fromStrict) wsRespEv
 
-home :: DomBuilder t m => m ()
-home = undefined
+home :: ( DomBuilder t m, PostBuild t m, Prerender js m, PerformEvent t m, MonadHold t m
+        , TriggerEvent t m
+        ) => m ()
+home = do
+  pb <- getPostBuild
+  let opts :: ChartOptions = undefined
+  let mkTs s = fromJust $ parseTimeM True defaultTimeLocale "%FT%R" s :: UTCTime
+  let chartData = 
+          fromList [ ("success"
+                        , ( def & series_smooth ?~ Left True & series_name ?~ "success"
+                          , 15 :: Int
+                          , [(mkTs "2018-01-01T05:03", 1.0 :: Double)] <$ pb))
+                   , ("fail"
+                        , ( def & series_smooth ?~ Left True & series_name ?~ "fail"
+                          , 15 :: Int
+                          , [(mkTs "2018-01-01T05:03", 2.0 :: Double)] <$ pb))
+                     ]
+  prerender blank $  void $ do
+    timeLineChart $ TimeLineChartConfig (600, 400) (constDyn opts) chartData
 
 frontend :: Frontend (R FrontendRoute)
 frontend = Frontend
