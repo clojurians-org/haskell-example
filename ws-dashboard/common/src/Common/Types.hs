f8ef0f0980d6abecfed243238220cd8f4ca1e618
clojurians-org <larluo@clojurians.org>
2019-05-27 18:12:42 +0800

    add websocket integration

--- ws-dashboard/common/src/Common/Types.hs
+++ ws-dashboard/common/src/Common/Types.hs
@@ -1,15 +1,19 @@
 {-# LANGUAGE DeriveGeneric #-}
 module Common.Types where
 
-import Data.Aeson (FromJSON, ToJSON)
+import Data.Aeson (FromJSON(..), genericParseJSON, defaultOptions, fieldLabelModifier)
 import GHC.Generics (Generic)
 import Data.Time (UTCTime)
+import Data.Maybe (fromJust)
+import Data.List (stripPrefix)
 
 data ApiStat = ApiStat
-  { _apiStat_success_num :: Int
+  { _apiStat_ts :: UTCTime
+  , _apiStat_success_num :: Int
   , _apiStat_fail_num :: Int
-  , _apiStat_ts :: UTCTime
   } deriving (Show, Generic)
 
-instance FromJSON ApiStat
-instance ToJSON ApiStat
+instance FromJSON ApiStat where
+  parseJSON = genericParseJSON defaultOptions {
+      fieldLabelModifier = fromJust . stripPrefix "_apiStat_"
+    }
